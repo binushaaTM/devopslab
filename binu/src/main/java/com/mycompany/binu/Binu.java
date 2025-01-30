b2e166dc05c1b99d84432287ba1d90247d120f41
binushaaTM <155132083+binushaaTM@users.noreply.github.com>
2025-01-30 14:34:54 +0530

    Add files via upload

---
+++ binu/src/main/java/com/mycompany/binu/Binu.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.binu;
+import java.util.*;
+public class Binu {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the number 1:");
+       int a=sc.nextInt();
+           System.out.println("Enter the number 2:");
+              
+         int b=sc.nextInt();
+            int c= a+b;  
+     
+         
+      
+        System.out.println("The addtion of 2 num is"+ c);
+       
+    }
+}
